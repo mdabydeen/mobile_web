0fa9e7c8ebbb9782c768030ebc1beda74cf64347
Daniel Audino <audinod@sheridanc.on.ca>
2012-08-09 14:38:53 -0400

    Added to style for table, updates to Data

--- data.js
+++ data.js
@@ -12,22 +12,23 @@
 
 //If xml file retreval fails display failure
 function failedxml(){
-	$("#content").append('Error retreving XML');
+	$("table").append('Error retreving XML');
 }
 
 function handleResponse(xml) {
 	s=0;
+	$("table").append("<tr><th>Country</th><th>Feild Of Education</th><th>Year</th><th>Values</th></tr>");
 	//Go through each record
 	$(xml).find("record").each(function()
        	{
+	record = $(this);
 		//Check if first item parsed
 		if(s == 0){
 			//initialize the variables
-			$("#content").append("<table border=\"1\">");
 			country = $(this).find("country").text();
 			feild = $(this).find("field_of_education").text();
-  			$("#content").append("<tr><td>"+$(this).find("country").text()+"</td>");
-			$("#content").append("<td>"+$(this).find("field_of_education").text()+"</td>");
+  			$("table").append("<tr><td>"+$(this).find("country").text()+"</td>");
+			displayTable(this);
 			//update (not first item parsed)
 			s++;
 		}
@@ -35,22 +36,25 @@ function handleResponse(xml) {
 			//Check if country and feild are same if so add new values to list
 			if(country == $(this).find("country").text()){
 				if(feild==$(this).find("field_of_education").text()){
-					$("#content").append("<td>FAIKSJHFS"+$(this).find("field_of_education").text()+"</td>");			
+					displayTable(record);
 				}
 				else{
-					$("#content").append("</tr><td></td><tr><td>"+$(this).find("field_of_education").text()+"</td>");
-					feild = $(this).find("field_of_education").text();	
+					feild = $(this).find("field_of_education").text();
+					displayTable(record)	
 				}
 			}
 			else{
-				$("#content").append("</tr><tr><td>"+$(this).find("country").text()+"</td>");
-				$("#content").append("<td>"+$(this).find("field_of_education").text()+"</td>");
+				$("table").append("<tr><td>"+$(this).find("country").text()+"</td>");
+				displayTable($(this));
 				country = $(this).find("country").text();
 				feild = $(this).find("field_of_education").text();
 			}
 		}
 	});
-	$("#content").append("</table>");
+	function displayTable(record){
+		$("table").append("<tr><td></td><td>"+$(record).find("field_of_education").text()+"</td><td>"+$(record).find("year").text()+"</td><td>"+$(record).find("value").text()+"</td></tr>");
+}
+
 }
 
    
